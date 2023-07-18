d705d79c42afc0a40f8aa0c5d4dac0343c57f119
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2023-07-18 17:11:57 -0300

    graphql go

---
+++ internal/database/course.go
@@ -0,0 +1,63 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryID  string
+}
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{db: db}
+}
+
+func (course *Course) Create(name string, description string, categoryID string) (*Course, error) {
+	id := uuid.New().String()
+	_, err := course.db.Exec("INSERT INTO courses (id, name, description, category_id) VALUES ($1, $2, $3, $4)", id, name, description, categoryID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Course{ID: id, Name: name, Description: description, CategoryID: categoryID}, nil
+}
+
+func (course *Course) FindAll() ([]Course, error) {
+	rows, err := course.db.Query("SELECT id, name, description, category_id FROM courses")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	courses := []Course{}
+	for rows.Next() {
+		var id, name, description, categoryID string
+		if err := rows.Scan(&id, &name, &description, &categoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: categoryID})
+	}
+	return courses, nil
+}
+
+func (course *Course) FindByCategoryId(categoryID string) ([]Course, error) {
+	rows, err := course.db.Query("SELECT id, name, description, category_id FROM courses WHERE category_id = $1", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	courses := []Course{}
+	for rows.Next() {
+		var id, name, description, categoryID string
+		if err := rows.Scan(&id, &name, &description, &categoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Course{ID: id, Name: name, Description: description, CategoryID: categoryID})
+	}
+	return courses, nil
+}
